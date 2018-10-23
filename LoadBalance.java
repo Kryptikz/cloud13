d8d1dae65f8a164fef59e11185d4c8ccb2277228
Tristan Lonswy <tblonswa@syr.edu>
2018-10-23 15:04:19 -0400

    Started work on load balancing server

---
+++ LoadBalance.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+public class LoadBalance implements Runnable {
+    ArrayList<Socket> centrals = new ArrayList<Socket>();
+    ServerSocket ss;
+    String[] central_ips = new String[]{"1.1.1.1","2.2.2.2","3.3.3.3"};
+    int port = 53136;
+    public LoadBalance() {
+        try {
+            for(String s : central_ips) {
+                centrals.add(new Socket(s,port));
+            }
+            ss = new ServerSocket(53135);
+        } catch (Exception e) {
+            System.out.println("ERROR ESTABLISHING SERVER: " + e.toString());   
+        }
+        run();
+    }
+    public void run() {
+        while(true) {
+            Socket s;
+            try {
+                s = ss.accept();
+                //check all central servers for lowest load, then send that IP
+                
+                
+            } catch (Exception e) {
+                System.out.println("ERROR GETTING CONNECTION: " + e.toString());
+            }
+        }
+        
+        
+    } 
+}
