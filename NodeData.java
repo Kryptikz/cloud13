3c3b10fb3f4ccb3fca8b597355ba20298a5ae025
Brian Hulbert <Brian.hulbert@me.com>
2018-10-23 17:57:46 -0400

    NodeManager class and NodeData class

---
+++ NodeData.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class NodeData{
+    ArrayList<String> fileNames;
+    //ArrayList<BlockData> data;
+    public NodeData(){
+        fileNames = new ArrayList<String>();
+        //data = new ArrayList<BlockData>();
+    }
+}
---
+++ NodeManager.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+public class NodeManager{
+    ArrayList<NodeData> nodes;
+    public NodeManager(){
+        nodes = new ArrayList<NodeData>();
+    }
+}
